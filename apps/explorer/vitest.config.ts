f13d9e8123679389a0307778548b2ba3a1dd4b54
Jibz <godwinjibs@gmail.com>
2022-11-28 11:10:56 -0500

    update

--- apps/explorer/vitest.config.ts
+++ apps/explorer/vitest.config.ts
@@ -1,9 +1,11 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
+import { defineConfig } from 'vitest/config';
 
 import tsconfig from './tsconfig.json';
 
@@ -14,12 +16,13 @@ Object.entries(tsconfig.compilerOptions.paths).forEach(([key, [value]]) => {
     tsconfigPaths[key] = alias(value);
 });
 
-// Assign the Vercel Analytics ID into a vite-safe name:
-process.env.VITE_VERCEL_ANALYTICS_ID = process.env.VERCEL_ANALYTICS_ID;
-
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), svgr()],
+    plugins: [react(), svgr()] as any,
+    test: {
+        globals: true,
+        environment: 'happy-dom',
+    },
     build: {
         // Set the output directory to match what CRA uses:
         outDir: 'build',
